b5e85595b83196039973bcffa36464ffdc509d9f
Akhil2279 <akhilakhi2528@gmail.com>
2026-02-19 17:36:16 +0530

    iterables_iterators

--- src/iterables_iterators/driver.py
+++ src/iterables_iterators/driver.py
@@ -4,3 +4,5 @@ from util import probability_of_a
 if __name__ == "__main__":
     result = probability_of_a()
     print(result)
+
+
--- src/iterables_iterators/util.py
+++ src/iterables_iterators/util.py
@@ -23,3 +23,5 @@ def probability_of_a():
 if __name__ == '__main__':
     result = probability_of_a()
     print(result)
+
+    
